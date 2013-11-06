7f4f6b068cfac709cee4b51754d0a8b57753337a
Jon Snider <jon@bowensnider.net>
2013-11-06 09:35:42 -0500

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -48,6 +48,16 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_title('Sign in') }
         end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
       end
 
       describe "as wrong user" do
@@ -58,12 +68,12 @@ describe "Authentication" do
         describe "submitting a GET request to the Users#edit action" do
           before { get edit_user_path(wrong_user) }
           specify { expect(response.body).not_to match(full_title('Edit user')) }
-          specify { expect(response).to redirect_to(home_url) }
+          specify { expect(response).to redirect_to(root_url) }
         end
 
         describe "submitting a PATCH request to the Users#update action" do
           before { patch user_path(wrong_user) }
-          specify { expect(response).to redirect_to(home_url) }
+          specify { expect(response).to redirect_to(root_url) }
         end
       end
 
@@ -91,6 +101,18 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(signin_path) }
         end
       end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end      
     end
   end
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,7 @@ describe "Static pages" do
   subject { page }
   
   describe "Home page" do
-    before {visit home_path }
+    before {visit root_path }
 
     it { should have_content('Sample App') }
     it { should have_title(full_title('')) }
@@ -17,13 +17,24 @@ describe "Static pages" do
         FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
         FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
         sign_in user
-        visit home_path
+        visit root_path
       end
 
       it "should render the user's feed" do
         user.feed.each do |item|
           expect(page).to have_selector("li##{item.id}", text: item.content)
         end
+      end
+
+      describe "follower/folowing counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
       end
     end
   end
